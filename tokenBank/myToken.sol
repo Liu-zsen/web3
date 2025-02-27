834ff4ac0741d9a5eec7b7fa5b1e2f252a337039
Liu-zsen <773853230@qq.com>
2025-02-27 15:54:45 +0800

    tokenBank合约增加Permit方式；myToken迭代

---
+++ tokenBank/myToken.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+
+// 创建一个新的合约， 继承支持标准 ERC20 和 EIP-2612 的 permit 功能
+contract MyToken is ERC20, ERC20Permit {
+    // 构造函数将初始化 ERC20 供应量和代币名称
+    constructor(uint256 initialSupply) 
+        ERC20("MyToken2", "MTK2") 
+        ERC20Permit("MyToken2") 
+    {
+        _mint(msg.sender, initialSupply);
+    }
+}
+
--- tokenBank/tokenBank.sol
+++ tokenBank/tokenBank.sol
@@ -2,58 +2,84 @@
 pragma solidity ^0.8.24;
 
 // 导入 OpenZeppelin 提供的 ERC20 标准合约
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Permit.sol";
+
+// sepolia: 0x1Cd149D60b15cD3F654f382F5f26D1FeDd225F82
 contract TokenBank {
     // 存储每个地址的 Token 余额
     mapping(address => mapping(address => uint256)) public balances;
 
-    // Token 合约地址
-    address public tokenAddress;
-
-    // 初始化时设置 Token 合约地址
-    constructor(address _tokenAddress) {
-        tokenAddress = _tokenAddress;
+    event PermitDeposited(address indexed user, address indexed token, uint256 amount);
+    // 事件：记录存款和取款操作
+    event Deposited(address indexed user, address indexed token, uint256 amount);
+    event Withdrawn(address indexed user, address indexed token, uint256 amount);
+    struct Permit {
+        address tokenAddress;
+        uint256 amount;
+        uint256 deadline;
+        uint8 v;
+        bytes32 r;
+        bytes32 s;
     }
 
+    // 初始化
+    constructor() { }
+
     // 存入 Token
-    function deposit(uint256 amount) external {
+    function deposit(address tokenAddress,uint256 amount) external {
         require(amount > 0, "Amount must be greater than 0");
+        require(tokenAddress != address(0), "Invalid token address");
 
         // 从用户地址转移 Token 到合约地址
         bool success = IERC20(tokenAddress).transferFrom(msg.sender, address(this), amount);
         require(success, "Transfer failed");
 
         // 更新用户的余额
-        balances[tokenAddress][msg.sender] += amount;
+        balances[msg.sender][tokenAddress] += amount;
+        
+        emit Deposited(msg.sender, tokenAddress, amount);
     }
 
     // 提取 Token
-    function withdraw(uint256 amount) external {
+    function withdraw(address tokenAddress, uint256 amount) external {
         require(amount > 0, "Amount must be greater than 0");
-        require(balances[tokenAddress][msg.sender] >= amount, "Insufficient balance");
+        require(balances[msg.sender][tokenAddress] >= amount, "Insufficient balance");
 
         // 更新用户的余额
-        balances[tokenAddress][msg.sender] -= amount;
+        balances[msg.sender][tokenAddress] -= amount;
 
         // 从合约地址转移 Token 到用户地址
         bool success = IERC20(tokenAddress).transfer(msg.sender, amount);
         require(success, "Transfer failed");
+        emit Withdrawn(msg.sender, tokenAddress, amount);
+    }
+
+    /**
+     * 增加 permit 离线签名授权的存款函数
+     *  data:
+        *  token：代币地址。
+            amount：存款数量。
+            deadline：签名有效截止时间。
+            v, r, s：离线签名的组成部分。
+     */
+    function permitDeposit(Permit calldata data) external {
+        require(data.amount > 0, "Amount must be greater than 0");
+        require(data.tokenAddress != address(0), "Invalid token address");
+
+        // permit 使用离线签名授权 TokenBank 转移代币 
+        IERC20Permit(data.tokenAddress).permit(msg.sender, address(this), data.amount, data.deadline, data.v, data.r, data.s);
+        IERC20 token = IERC20(data.tokenAddress);
+        // 从调用者转移代币到合约
+        require(token.transferFrom(msg.sender, address(this), data.amount), "Transfer failed");
+        // 更新用户在该代币中的余额
+        balances[msg.sender][data.tokenAddress] += data.amount;
+        emit PermitDeposited(msg.sender, data.tokenAddress, data.amount);
     }
 
     // 获取用户的 Token 余额
-    // function getBalance(address user) external view returns (uint256) {
-    //     return balances[tokenAddress][user];
-    // }
-}
-
-
-// 创建一个新的合约，继承自 OpenZeppelin 的 ERC20 合约
-contract MyToken is ERC20 {
-    // 构造函数将初始化 ERC20 供应量和代币名称
-    constructor(uint256 initialSupply) ERC20("MyToken", "MTK") {
-        // 通过 _mint 函数铸造初始供应量的代币到部署合约的地址
-        _mint(msg.sender, initialSupply);
+    function getBalance(address user, address tokenAddress) external view returns (uint256) {
+        return balances[user][tokenAddress];
     }
 }
